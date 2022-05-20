08c260fb3d9a262d78a43b5daa5ad7aa5489aeb9
azza <1978328591@qq.com>
2022-05-20 11:14:36 +0800

    Initial commit

---
+++ Assets/Scripts/SampleScripts.cs
@@ -0,0 +1,23 @@
+//------------------------------------------------------------
+// ACGGAME
+// Copyright © 2022-2023 PengCheng Wang. All rights reserved.
+//------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
