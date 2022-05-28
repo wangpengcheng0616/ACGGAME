99d9d1144ed06842c5017520ababadfc608ff349
azza <1978328591@qq.com>
2022-05-28 11:12:35 +0800

    Update SampleScripts.cs

--- Assets/Scripts/SampleScripts.cs
+++ Assets/Scripts/SampleScripts.cs
@@ -3,21 +3,14 @@
 // Copyright © 2022-2023 PengCheng Wang. All rights reserved.
 //------------------------------------------------------------
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SampleScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Start
+    /// </summary>
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
